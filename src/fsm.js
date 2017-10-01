fcf2facc415c8a493e2d96caf04cb92051eec7c7
Looofeee <Looofeee@mail.ru>
2017-10-01 21:15:43 +0300

    V1

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,49 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+    	if (config === undefined) {
+    		throw new Error;
+    	}
+    	this.config = config;
+    	this.clear = 0;
+    	this.state = 'normal';
+    	
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+    	if (this.state )
+    	return this.state;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+    		this.state = state;
+    	}
+
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+    	this.event = event;
+
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+    	this.state = 'normal';
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +53,52 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+    	var a = [];
+    	a = Object.keys(this.config.states);
+    	if (event === undefined) {
+    		return a
+    	} else if (0) {
+    		
+    	} else if (0) {
+
+    	} else {
+    		return [];
+    	}
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+    	if (this.clear === 1) {
+    		return false;
+    	} else {
+    		return true;
+    	}
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+    	if (this.clear === 1) {
+    		return false;
+    	} else {
+    		return true;
+    	}
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+    	this.clear = 1;
+    }
 }
 
 module.exports = FSM;
